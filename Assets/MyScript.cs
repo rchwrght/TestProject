c597236904cdc8c5d2f9afc7d69efcc2d9370426
Rich Wright <rchwrght@gmail.com>
2017-10-15 15:00:03 +0200

    Revert "Added more script"
    This reverts commit 3e8cf63864c4cc421e72eca31c94b03eb5b4d67f.

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -7,7 +7,6 @@ public class MyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("VCS test");
-        print("Added extra Line");
 	}
 	
 	// Update is called once per frame
